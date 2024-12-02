0514e24ef69d8f43d0677e3cc614dd5e424522fe
sKzAy <1mkzaexe@gmail.com>
2024-12-02 14:34:22 +0500

    changes

--- components/main.js
+++ components/main.js
@@ -8,7 +8,7 @@ const Main = () => {
             <h1 className='text-white text-7xl text-center pt-10 md:text-[10rem]'>Kazim Ali</h1>
         </div>
         <div className=' text-white '>
-            <p className='text-sm text-center mb-2 md:text-left md:w-[40vw] md:mx-auto'>Hi, I am an aspiring Web Developer from Pakistan.</p>
+            <p className='text-sm text-center mb-2 md:w-full md:mx-auto'>Hi, I am an aspiring Web Developer from Pakistan.</p>
         </div>
         <Card/>
     </>
