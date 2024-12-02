bb2bb85a8f32c0f775246524e287b3ab21268fa1
sKzAy <1mkzaexe@gmail.com>
2024-12-02 14:21:18 +0500

    yayyyy

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ import "./globals.css"
 export default function Home() {
   return (
     <>
-    <div className="md:hidden bg h-full overflow-clip">
+    <div className="md:hidden bg h-full ">
     <Main/>
     </div>
-    <div className="max-md:hidden bg2 h-full overflow-clip">
+    <div className="max-md:hidden bg2 h-full ">
     <Main/>
     </div>
     </>
